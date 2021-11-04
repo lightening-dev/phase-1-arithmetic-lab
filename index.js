bf9d11a1672388d9a035bc13ec343c40786461c3
Lightening Gibson <lighteningibson@gmail.com>
2021-11-04 13:02:21 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random()* 10) +1 ;
+
+
+
+
+
+let num3 = 100;
+let num4 = 25;
+let mod = (num3 / num4);
+
+let max = (1,2,3,4,5,20);
+
+Math.max(max);
+
