f0d8a7de59f77e63831560c6544616b4f7b3b21c
Lightening Gibson <lighteningibson@gmail.com>
2022-05-29 16:10:50 -0500

    commit tree ca875d54cf5453167bc67f66312db47e2b7e88a8

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 
 // Write your code here
-let num1 = 1;
-let num2 = 62;
+let num1 = 2
+let num2 = 31
 
-let multiply = (num1 * num2);
-
-let random = Math.floor(Math.random()* 10) +1 ;
+var multiply = (num1 * num2)
 
 
+var random = Math.ceil(Math.random() * 10);
 
+let num3 = 100
+let num4 = 25
+var mod = (num3/num4);
 
-
-let num3 = 100;
-let num4 = 25;
-let mod = (num3 / num4);
-
-let max = (1,2,3,4,5,20);
-
-Math.max(max);
+var max = Math.max(2, 3, 4, 20);
 
